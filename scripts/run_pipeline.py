4fb1cfa6eea6163371ad15939c94199481bb8760
MiniEma <emanueleminiaci7@gmail.com>
2025-04-03 17:27:10 +0200

    Update run_pipeline.py

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.append(os.path.abspath('..'))  # Adds the parent directory to sys.path
+sys.path.append(os.path.abspath('..')) 
 
 import logging
 import src
@@ -8,8 +8,7 @@ from src import config
 from src.load_data import load_data
 from src.preprocess import preprocess_data
 from src.make_model import train_model
-# from src.evaluation import evaluate_model
-# from src.save_results import save_predictions
+
 
 # Set up logging
 logging.basicConfig(filename='../log/pipeline.log', level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
